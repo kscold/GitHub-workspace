c8d25a72d0ab4641dc10d2db476eeaaf892d614e
kscold <chan6502@gmail.com>
2023-04-07 11:00:00 +0000

    HTML update

---
+++ HTML/javascriptpractice/ex01.js
@@ -0,0 +1,30 @@
+// document.write('<p>JavaScript는 재밌어:)</p>');
+
+// var num;
+// 변수 선언
+
+// 변수 초기화
+// num = 10;
+
+// 변수 선언 + 초기화
+// var num = 10;
+// num = 20;
+
+// console.log(num);
+// 크롬브라우저 개발자 모드에서 로그를 확인할 수 있음
+
+// 사용할 수 없는 변수명
+// const container = document. getElementById('brand-title');
+
+// var el = document.getElementById("brand-title");
+
+// console.log(el);
+
+// //console.log(el.innerHTML);
+// console.log(el.innerText);
+
+// el.innerText = "안녕하세요 :)";
+
+var el = document.getElementsByClassName('brand-title');
+console.log(el);
+// 요소의 속성값을 확인
