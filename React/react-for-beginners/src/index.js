b895ae41fcebb0cad17cafaf8b0903d4ee49a12a
kscold <chan6502@gmail.com>
2023-05-08 22:28:53 +0900

    React update

--- React/react-for-beginners/src/index.js
+++ React/react-for-beginners/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+//import "./styles.css"; 기본적인 틀에 가두므로 사용하지 않음 따라서 모듈방식으로 사용
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,4 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
