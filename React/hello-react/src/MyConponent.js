33ebeea9595c90dfef72229be6c289a79b234681
kscold <chan6502@gmail.com>
2023-05-10 21:52:36 +0900

    React update

---
+++ React/hello-react/src/MyConponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyConponent = (props) => {
+    return (
+        <div>
+            Hi, my name is {props.name}!
+        </div>
+    );
+};
+
+export default MyConponent;
