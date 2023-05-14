f6acd03d3a7600d44efb03da0cbbf4d6d3ec4c40
kscold <chan6502@gmail.com>
2023-05-14 22:33:35 +0900

    C/C++ update

--- C/practice.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    int num = 3;
-    printf("%d", num);
-    return 0;
-}
---
+++ C\C++/리눅스/lec05_03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(){
+    int fdin, fdout;
+    ssize_t nread, nwirte;
+    char buffer[1024];
+
+    fdin =open("fdin.txt",O_RDONLY);
+    fout = open("fdout.txt",O_WRONLY | O_CREAT |O_TRUNC. 0644);
+
+    printf("file descriptor: %d\n", fdin);
+    printf("file descriptor: %d\n", fdout);
+}
