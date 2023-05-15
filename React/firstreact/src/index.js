5de3b6085260257d6fe7a580be9079b97ce80332
kscold <chan6502@gmail.com>
2023-05-16 00:32:20 +0900

    React update

--- React/firstreact/src/index.js
+++ React/firstreact/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import CommentList from './chapter_05/CommentList';
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CommentList />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
