534c6e7c7139476b76b7ab88fed841b9a7777a50
kscold <chan6502@gmail.com>
2023-05-19 17:16:33 +0900

    React update

--- React/bookreact/src/App.js
+++ React/bookreact/src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  const name = '리액트';
-  return (
-    <>
-      <div className="react">{name}</div>
-      <input />
-    </>
-  );
-}
+//import MyComponent from "./MyComponent";
+import Counter from './Counter';
+import Say from './Say';
+import EventPractice from './EventPractice';
+
+const App = () => {
+  return <EventPractice />
+  //return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>//컴포넌트 태그 사이의 내용을 보여주는 props를 children이라고 함
+  //props 지정할려면 name =""
+  //현재 string타입으로 PropTypes를 지정해놓았으므로 {3}이면 오류
+  //favoriteNumber를 PropTypes.number.isRequired로 지정해놓았기 때문에 파라미터가 없으면 오류 메시지를 띄움
+};
 
 export default App;
