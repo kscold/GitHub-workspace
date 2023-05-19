9428a50d68fd8823ca90b4e4bd5ff2d50cbe6e43
kscold <chan6502@gmail.com>
2023-05-19 23:07:23 +0900

    React Update

--- React/bookreact/src/App.js
+++ React/bookreact/src/App.js
@@ -4,9 +4,10 @@ import React from 'react';
 import Counter from './Counter';
 import Say from './Say';
 import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
 
 const App = () => {
-  return <EventPractice />
+  return <ValidationSample />
   //return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>//컴포넌트 태그 사이의 내용을 보여주는 props를 children이라고 함
   //props 지정할려면 name =""
   //현재 string타입으로 PropTypes를 지정해놓았으므로 {3}이면 오류
