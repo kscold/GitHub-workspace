52966c5208a89ffa22d58790cff11a126ef56b6d
kscold <chan6502@gmail.com>
2023-05-20 16:29:35 +0900

    React Update

--- React/firstreact/src/index.js
+++ React/firstreact/src/index.js
@@ -4,12 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import CommentList from './chapter_05/CommentList';
-import NotificationList from './chapter_06/NotificationList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NotificationList />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
