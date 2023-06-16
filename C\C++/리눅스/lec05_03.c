9ae232b230065330df1a19ae081a3022adf2ef5a
kscold <chan6502@gmail.com>
2023-06-16 17:47:29 +0900

    C Update

--- C\C++/리눅스/lec05_03.c
+++ C\C++/리눅스/lec05_03.c
@@ -2,13 +2,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(){
+int main()
+{
     int fdin, fdout;
     ssize_t nread, nwirte;
     char buffer[1024];
 
-    fdin =open("fdin.txt",O_RDONLY);
-    fout = open("fdout.txt",O_WRONLY | O_CREAT |O_TRUNC. 0644);
+    fdin = open("fdin.txt", O_RDONLY);
+    fdout = open("fdout.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
     printf("file descriptor: %d\n", fdin);
     printf("file descriptor: %d\n", fdout);
--- C\C++/리눅스/시스템프로그래밍/pthread.c
+++ C\C++/리눅스/시스템프로그래밍/pthread.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <pthread.h>
-#define NUM_THREADS 4
+#define NUM_THREADS 4 // 스레드 지정
 
-void *print_helloworld(void *arg)
+void *print_helloworld(void *arg) // 함수 선언의 void *는 함수가 모든 유형의 포인터를 인수로 받아들일 수 있다.
 {
     printf("Hello World!!\n");
     return 0;
@@ -12,6 +12,7 @@ int main()
 {
     int i = 0;
     pthread_t thread_arr[NUM_THREADS];
+
     for (i = 0; i < NUM_THREADS; i++)
         pthread_create(&thread_arr[i], NULL, print_helloworld, NULL); // 4개의 스레드 생성 및 화면 출력 실행
     for (i = 0; i < NUM_THREADS; i++)
