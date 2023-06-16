a916adbc02f8752f09806b2f4c711c5195fa9770
kscold <chan6502@gmail.com>
2023-06-16 17:47:29 +0900

    C Update

--- C\C++/리눅스/시스템프로그래밍/loop_parallel.c
+++ C\C++/리눅스/시스템프로그래밍/loop_parallel.c
@@ -2,3 +2,26 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
+
+int main(void)
+{
+    double *data;
+    int data_size = 100000000;
+    int i = 0;
+
+    data = (double *)malloc(sizeof(double));
+
+    for (i = 0; i < data_size; i++)
+        data[i] = i;
+
+#pragma omp parallel num_threads(2)
+    {
+        for (i = 0; i < data_size; i++)
+            data[i] = sqrt(data[i]);
+    }
+
+    printf("data: %f, %f, %f, %f, %f\n", data[0], data[1], data[2], data[3], data[4]);
+
+    free(data);
+    return 0;
+}
