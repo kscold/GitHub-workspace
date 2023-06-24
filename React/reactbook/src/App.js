06c11022f326af8c66fd317f30c6f37e408ff02b
kscold <chan6502@gmail.com>
2023-06-24 22:08:15 +0900

    React Update

--- React/reactbook/src/App.js
+++ React/reactbook/src/App.js
@@ -1,19 +1,31 @@
-import { React, useState } from 'react';
-import Info from './Info';
+// import { React, useState } from 'react';
+// import Info from './Info';
+
+// const App = () => {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           setVisible(!visible);
+//         }}>
+//         {visible ? '숨기기' : '보이기'}
+//       </button>
+//       <hr />
+//       {visible && <Info />}
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+import Counter from './Counter'
+import Infoclass from './Infoclass'
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
   return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}>
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
+    <Infoclass />
   )
 }
 
