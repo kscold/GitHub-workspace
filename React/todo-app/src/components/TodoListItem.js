cde64337be0c1a94b4f9f7ff251b0d8f2467108c
kscold <chan6502@gmail.com>
2023-06-28 01:47:28 +0900

    React Update

--- React/todo-app/src/components/TodoListItem.js
+++ React/todo-app/src/components/TodoListItem.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
     MdCheckBoxOutlineBlank,
     MdCheckBox,
@@ -6,20 +7,22 @@ import {
 import cn from 'classnames' //React에서는 class가 예약어로 사용중이기 떄문에 이를 해결하기 위해 나온 라이브러리
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
     const { id, text, checked } = todo; //비구조화 할당으로 App.js에 있는 데이터를 변수에 저장 text = todo.text
 
     return (
-        <div className='TodoListItem'>
-            <div className={cn('checkbox', { checked })}>
-                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-                <div className='text'>{text}</div>
-            </div>
-            <div className='remove' onClick={() => onRemove(id)}>
-                <MdRemoveCircleOutline />
-            </div>
-        </div >
+        <div className="TodoListItem-virtualized" style={style}>
+            <div className='TodoListItem'>
+                <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+                    {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                    <div className='text'>{text}</div>
+                </div>
+                <div className='remove' onClick={() => onRemove(id)}>
+                    <MdRemoveCircleOutline />
+                </div>
+            </div >
+        </div>
     );
 };
 
-export default TodoListItem;
+export default React.memo(TodoListItem); //이전 랜더된 결과와 비교하여 DOM 업데이트를 결정한다.
