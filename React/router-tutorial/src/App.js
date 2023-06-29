778f3bfbb1ea0551d0c470835fc11b732bcb8a04
kscold <chan6502@gmail.com>
2023-06-29 19:50:33 +0900

    React Update

--- React/router-tutorial/src/App.js
+++ React/router-tutorial/src/App.js
@@ -5,6 +5,9 @@ import Article from "./pages/Article";
 import Articles from "./pages/Articles";
 import Home from './pages/Home';
 import Profile from "./pages/Profile";
+import MyPage from "./pages/MyPage"
+import NotFound from "./pages/NotFound";
+import Login from './pages/Login';
 
 const App = () => {
   return (
@@ -17,6 +20,10 @@ const App = () => {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />  {/* 자동적으로 articles/번호가 id에 매핑됨 */}
       </Route>  {/* /articles/:id가 아니라 Outlet 컴포넌트를 사용하여 이런식으로 children으로 만들어 주면 중텁괸 라우트가 보여짐*/}
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="*" element={<NotFound />} /> {/* *은 wildcard 무자로 아무 텍스트나 매칭한다는 뜻 라우트 엘리먼트 상단에 위치하는 라우트들의
+       모든 규칙을 확인하고 일치하는ㄴ 라우트가 없다면 이 라우트가 화면에 나타나게 됨 */}
     </Routes>
   );
 };
--- React/router-tutorial/src/pages/Articles.js
+++ React/router-tutorial/src/pages/Articles.js
@@ -1,22 +1,66 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom'; //css 속성을 적용하기 위해 NA
+//Link
 
 const Articles = () => {
     return (
         <div>
-            <Outlet /> {/* Article 컴포넌트에서 제공하는 Outlet 컴포넌트 Route의 children으로 들어감*/}
+            <Outlet />
             <ul>
-                <li>
-                    <Link to="/articles/1">게시글 1</Link>
-                </li>
-                <li>
-                    <Link to="/articles/2">게시글 2</Link>
-                </li>
-                <li>
-                    <Link to="/articles/3">게시글 3</Link>
-                </li>
+                <ArticleItem id={1} />
+                <ArticleItem id={2} />
+                <ArticleItem id={3} />
             </ul>
         </div>
     );
 };
+
+const ArticleItem = ({ id }) => {
+    const activeStyle = {
+        color: 'green',
+        fontSize: 21,
+    };
+
+    return (
+        <li>
+            <NavLink
+                to={`/articles/${id}`} //${id}를 통해서 코드를 최적화
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+                게시글{id}
+            </NavLink>
+        </li>
+    ) //<NavLink>를 통해서 반복되는 코드를 최적화
+}
+
+// return (
+//     <div>
+//         <Outlet /> {/* Article 컴포넌트에서 제공하는 Outlet 컴포넌트 Route의 children으로 들어감*/}
+//         <ul>
+//             <li>
+//                 <NavLink
+//                     to="/articles/1"
+//                     style={({ isActive }) => (isActive ? activeStyle : undefined)}
+//                 >
+//                     게시글 1
+//                 </NavLink>
+//             </li>
+//             <li>
+//                 <NavLink
+//                     to="/articles/2"
+//                     style={({ isActive }) => (isActive ? activeStyle : undefined)}
+//                 >
+//                     게시글 2</NavLink>
+//             </li>
+//             <li>
+//                 <NavLink
+//                     to="/articles/3"
+//                     style={({ isActive }) => (isActive ? activeStyle : undefined)}
+//                 >
+//                     게시글 3</NavLink>
+//             </li>
+//         </ul>
+//     </div>
+// );
+// };
 
 export default Articles;
