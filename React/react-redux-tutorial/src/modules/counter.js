2d1fc8717d940ace6219e4d0fcf988927cdc2dc0
kscold <chan6502@gmail.com>
2023-07-02 18:58:01 +0900

    React Update

--- React/react-redux-tutorial/src/modules/counter.js
+++ React/react-redux-tutorial/src/modules/counter.js
@@ -1,26 +1,40 @@
+import { createAction, handleActions } from "redux-actions";
+// import { increase } from '../containers/CounterContainer';
+// import counter from './counter';
+
 const INCREASE = 'counter/INCREASE' //액션 타입을 정의 모듈이름/액션이름
-const DECRAESE = 'counter/DECREASE'
+const DECREASE = 'counter/DECREASE'
 
-export const increase = () => ({ type: INCREASE }); //increase 객체가 type: INCREASE Key:value 값을 가짐 action은 type을 반드시 가짐
-export const decrease = () => ({ type: DECRAESE });
+// export const increase = () => ({ type: INCREASE }); //increase 객체가 type: INCREASE Key:value 값을 가짐 action은 type을 반드시 가짐
+// export const decrease = () => ({ type: DECRAESE });
 
-const initialState = {
-    number: 0
-}// 초기값 설정
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
-function counter(state = initialState, action) {  //초기 모델의 리듀서 함수를 만들어줌
-    switch (action.type) { //액션 타입
-        case INCREASE:
-            return {
-                number: state.number + 1 //state=initialState 초기값을 업데이트
-            };
-        case DECRAESE:
-            return {
-                number: state.number - 1
-            };
-        default:
-            return state;
+// const initialState = {
+//     number: 0,
+// }// 초기값 설정
+
+const counter = handleActions(
+    {
+        [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+        [DECREASE]: (state, action) => ({ number: state.number - 1 }),
     }
-}
+)
+
+// function counter(state = initialState, action) {  //초기 모델의 리듀서 함수를 만들어줌
+//     switch (action.type) { //액션 타입
+//         case INCREASE:
+//             return {
+//                 number: state.number + 1 //state=initialState 초기값을 업데이트
+//             };
+//         case DECRAESE:
+//             return {
+//                 number: state.number - 1
+//             };
+//         default:
+//             return state;
+//     }
+// }
 
 export default counter; //export는 여러개를 내보낼 수 있지만, export default는 단 한개만 내보낼 수 있음
