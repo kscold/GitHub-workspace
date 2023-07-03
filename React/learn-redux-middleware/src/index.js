b9b27604b446d8a13d55e0697455a680c6a04f0d
kscold <chan6502@gmail.com>
2023-07-04 02:21:17 +0900

    React Update

--- React/learn-redux-middleware/src/index.js
+++ React/learn-redux-middleware/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
+//import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const logger = createLogger(); //loogerMiddleware보다 훨신 잘 만들어진 라이브러리임
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- React/learn-redux-middleware/src/modules/index.js
+++ React/learn-redux-middleware/src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
+import sample from './sample';
 
 const rootReducer = combineReducers({ //combinReducers로 리듀서 .js를 묶음(switch 문이나 handleActions)
-    counter
+    counter,
+    sample
 });
 
 export default rootReducer;
