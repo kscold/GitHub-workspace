1d1ed8daa480cedae3f6ab2350dd5a3ada170ea9
kscold <chan6502@gmail.com>
2023-07-04 18:12:25 +0900

    Next Update

---
+++ Next/class/pages/section01/01-02-emotion/index.js
@@ -0,0 +1,12 @@
+export default function EmotionPage() {
+
+
+
+    return (
+        <div>
+            <span>이메일: </span>
+            <input type="text" />
+            <button>클릭하세요!</button>
+        </div>
+    )
+}
