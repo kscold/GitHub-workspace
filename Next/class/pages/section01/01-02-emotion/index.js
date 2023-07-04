a816196e744c0bde4fca5248cd1a330e5c96fec4
kscold <chan6502@gmail.com>
2023-07-04 23:59:12 +0900

    Next Update

--- Next/class/pages/section01/01-02-emotion/index.js
+++ Next/class/pages/section01/01-02-emotion/index.js
@@ -1,12 +1,15 @@
+import { MyEmail, MyEmailInput } from '../../../styles/01-02-emotion'
+
 export default function EmotionPage() {
 
 
 
-    return (
+    return ( //public이 아이콘을 저장하는 경로인데 기본적으로 <img> 태그의 /(root) 판정을 가지고 있음
         <div>
-            <span>이메일: </span>
-            <input type="text" />
+            <MyEmail>이메일: </MyEmail>
+            <MyEmailInput type="text" />
             <button>클릭하세요!</button>
+            <img src="/next.svg" />
         </div>
     )
 }
