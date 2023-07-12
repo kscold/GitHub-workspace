da1841fb6be80d5a0753332c39bae139312819cb
kscold <chan6502@gmail.com>
2023-07-12 19:31:59 +0900

    Next Update

--- Next/class/pages/section13/13-02-library-star/index.tsx
+++ Next/class/pages/section13/13-02-library-star/index.tsx
@@ -6,9 +6,9 @@ const App = (): JSX.Element => {
     const [value, setValue] = useState(3);
 
     // 1단계 방식
-    const onChangeStar = (value: number): void => {
-        setValue(value)
-    }
+    // const onChangeStar = (value: number): void => {
+    //     setValue(value) // 여기 value는 객체의 value만 
+    // }
 
     // 2단계 방식
     // const onChangeStar = (value) => setValue(value)
@@ -16,9 +16,10 @@ const App = (): JSX.Element => {
 
 
     return (
-        <Rate onChange={onChangeStar} value={value} />
+        // <Rate onChange={onChangeStar} value={value} />
         // 3단계 방식
         // <Rate onChange={(value) => setValue(value)} value={value} />
+        <Rate onChange={setValue} value={value} /> // 4단계 방식
     );
 };
 
---
+++ Next/class/src/components/commons/apollo/index.tsx
@@ -0,0 +1,18 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+interface IApolloSettingProps {
+    children: JSX.Element;
+}
+
+export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
+    const client = new ApolloClient({
+        uri: "http://practice.codebootcamp.co.kr/graphql",
+        cache: new InMemoryCache() // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아보기
+    }) // grapql 세팅
+
+    return (
+        <ApolloProvider client={client}>
+            {props.children}
+        </ApolloProvider>
+    )
+}
