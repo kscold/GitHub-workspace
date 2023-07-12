324eb700d8b200102b19ea0bfc26380c2fb81608
kscold <chan6502@gmail.com>
2023-07-12 22:42:52 +0900

    Next Update

--- Next/class/src/components/commons/apollo/index.tsx
+++ Next/class/src/components/commons/apollo/index.tsx
@@ -6,7 +6,7 @@ interface IApolloSettingProps {
 
 export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
     const client = new ApolloClient({
-        uri: "http://practice.codebootcamp.co.kr/graphql",
+        uri: "http://backend-practice.codebootcamp.co.kr/graphql",
         cache: new InMemoryCache() // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아보기
     }) // grapql 세팅
 
