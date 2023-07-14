224c2023424d59ea92fd88b2132d4eca6b5676b1
kscold <chan6502@gmail.com>
2023-07-14 17:39:16 +0900

    Next update

---
+++ Next/class_backend/decorator-example.ts
@@ -0,0 +1,11 @@
+function bbb(qqqqqqqq: any) {
+  console.log("==========")
+  console.log(qqqqqqqq)
+  console.log("==========")
+}
+
+@bbb // bbb라는 함수에 인자로 들어가게 된다
+class Board {
+  // number: number
+  // writer: string
+}
