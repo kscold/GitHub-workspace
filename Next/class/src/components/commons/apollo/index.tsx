7d3e03dd850da9fb3f13bca85698e82deedf9609
kscold <chan6502@gmail.com>
2023-07-17 17:42:23 +0900

    Next Update

--- Next/class/src/components/commons/apollo/index.tsx
+++ Next/class/src/components/commons/apollo/index.tsx
@@ -1,23 +1,26 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, ApolloLink } from '@apollo/client'
-import { createUploadLink } from 'apollo-upload-client'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+} from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
+
+const GLOBAL_STATE = new InMemoryCache();
 
 interface IApolloSettingProps {
-    children: JSX.Element;
+  children: JSX.Element;
 }
 
 export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
-    const uploadLink = createUploadLink({
-        uri: "http://backend-practice.codebootcamp.co.kr/graphql",
-    })
+  const uploadLink = createUploadLink({
+    uri: "http://backend-practice.codebootcamp.co.kr/graphql",
+  });
 
-    const client = new ApolloClient({
-        link: ApolloLink.from([uploadLink]),
-        cache: new InMemoryCache() // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아보기
-    }) // grapql 세팅
+  const client = new ApolloClient({
+    link: ApolloLink.from([uploadLink]),
+    cache: GLOBAL_STATE, // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아보기
+  }); // grapql 세팅
 
-    return (
-        <ApolloProvider client={client}>
-            {props.children}
-        </ApolloProvider>
-    )
+  return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
 }
