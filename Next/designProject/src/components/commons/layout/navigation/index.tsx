9bf3b35f0b9cbd504631dfa9fec1f2faf5513f67
kscold <chan6502@gmail.com>
2023-07-20 03:59:52 +0900

    Next Update

--- Next/designProject/src/components/commons/layout/navigation/index.tsx
+++ Next/designProject/src/components/commons/layout/navigation/index.tsx
@@ -1,10 +1,11 @@
-import styled from "@emotion/styled"
+import styled from "@emotion/styled";
 
 const Wrapper = styled.div`
-    height: 50px;
-    background-color: lightgreen;
-`
+  display: flex;
+  height: 50px;
+  background-color: lightgreen;
+`;
 
 export default function LayoutNavigation(): JSX.Element {
-    return <Wrapper>여기는 네비게이션입니다</Wrapper>
+  return <Wrapper>여기는 네비게이션입니다</Wrapper>;
 }
