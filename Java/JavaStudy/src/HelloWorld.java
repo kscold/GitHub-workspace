5de94612bd62c7e5167a6d65b364462a3e1bd0a4
kscold <chan6502@gmail.com>
2023-09-06 00:46:34 +0900

    Java Update

---
+++ Java/JavaStudy/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello, World!"); // out은 출력 장치 따라서 out을 붙인 다음에 println()을 설정
+        System.out.println("Hello, World"); // sout라는 인텔리제이 약어로 빠르게
+    }
+}
