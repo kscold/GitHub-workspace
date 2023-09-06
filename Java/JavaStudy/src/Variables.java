9cf7dad18724676e47512d68af91d4590e5a7895
kscold <chan6502@gmail.com>
2023-09-06 23:23:57 +0900

    JAVA update

---
+++ Java/JavaStudy/src/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+
+    // 변수 => 데이터가 저장되는 공간
+
+    // 1. 1개의 변수 => 1개의 값만 할당 가능
+    // 2. 변수 저장된 값 => 재할당을 통해 변경 가능
+    // 3. 값의 형태에 맞는 자료형을 사용
+
+    public static void main(String[] args) {
+        int num = 1;
+        double width = 12.34;
+        String content = "Programming"; // String의 경우 주소값으로 참조가 됨
+
+        num = 10;
+        System.out.println(num);
+
+        String
+    }
+}
