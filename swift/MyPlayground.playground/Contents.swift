357a65028e966113580725bf3a4f2ee7d3692a3f
kscold <chan6502@gmail.com>
2023-10-21 21:46:38 +0900

    Spring Update

--- swift/MyPlayground.playground/Contents.swift
+++ swift/MyPlayground.playground/Contents.swift
@@ -1,14 +1,30 @@
 import Swift
 
+class Person {
+    var height: Float = 0.0
+    var weight: Float = 0.0
+    
+    init() { // 기본 생성자 재정의
+        self.height = 10.0
+        self.weight = 20.0
+    }
+    deinit { // 소멸자 재정의
+        print("Person class instance is deinitialized")
+    }
+}
 
+var aPerson: Person? = Person()
+print(aPerson) // 실행 결과: Optional(__lldb_expr_13.Person)
 
-//  튜플 변수 선언 및 초기화
-var personA: (String, Int, Double) // unnamed
+//aPerson.height = 1.0 // 에러 발생: aPerson를 unwrapping해야 height에 접근 가능
+//print(aPerson.height) // 에러 발생: aPerson를 unwrapping해야 height에 접근 가능
 
-var personB: (name: String, age: Int, height: Double) // named
+if let bPerson = aPerson { // optional unwrapping
+    bPerson.height = 0.0
+    print(bPerson.height) // 실행 결과: 0.0
+}
 
-personA = ("joe", 10, 150.23)
-personB = (name: "joe", age: 10, height: 150.23)
-personB = ("joe", 12, 150.23) // 이렇게 해도 튜플은 오류 안남
+var cPerson: Person! = Person() // implicitly unwrapped optional
 
-print(personB)
+cPerson.height = 1.0
+print(cPerson.height) // 실행 결과: 1.0
