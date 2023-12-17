7489155870d365f6ab8cc7fe4051436f330d092f
kscold <chan6502@gmail.com>
2023-12-17 22:50:54 +0900

    java Update

---
+++ Java/java-start/src/operator/operator2.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class operator2 {
+    public static void main(String[] args) {
+
+        // 문자열과 문자열 더하기1
+        String result1 = "hello" + "world";
+        System.out.println(result1);
+
+        // 문자열과 문자열 더하기2
+        String result2 = "hello" + "world";
+        System.out.println(result1);
+
+    }
+}
---
+++ Java/java-start/src/variable/Var.java
@@ -0,0 +1,32 @@
+package variable;
+
+public class Var {
+    public static void main(String[] args) {
+      /*  // 1. 변수 선언, 초기화 각각 따로
+        int a;
+        a = 1;
+
+        System.out.println(a);
+
+        // 2. 변수 선언과 초기화를 한번에
+        int b = 2;
+        System.out.println(b);
+
+        int c = 3, d = 4; // 3. 여러 변수 선언과 초기화를 한번에
+        System.out.println(c);
+        System.out.println(d);*/
+
+        int a = 100; // 정수
+        double b = 10.5; // 실수
+        boolean c = true; // 불리런(boolean) true, false 입력 가능
+        char d = 'A'; // 문자 하나
+        String e = "Hello Java"; // 문자열, 문자열을 다루기 위한 특별한 타입
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
