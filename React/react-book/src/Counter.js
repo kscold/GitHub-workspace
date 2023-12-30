1cfae917d97f7465123671ee81d068acffa74ab5
kscold <chan6502@gmail.com>
2023-12-30 18:12:52 +0900

    React Update

--- React/react-book/src/Counter.js
+++ React/react-book/src/Counter.js
@@ -1,15 +1,73 @@
+// import React, { Component } from "react";
+
+// class Counter extends Component {
+//   constructor(props) {
+//     super(props); // props를 오버라이딩
+
+//     // state의 초깃값 설정하기
+//     this.state = {
+//       number: 0,
+//       fixedNumber: 0,
+//     };
+//   }
+
+//   render() {
+//     const { number, fixedNumber } = this.state;
+//     return (
+//       <div>
+//         <h1>{number}</h1>
+//         <h2>바뀌지 않는 값: {fixedNumber}</h2>
+//         <button
+//           onClick={() => {
+//             this.setState({ number: number + 1 }); // this.setState를 사용하여 state에 새로운 값을 넣을 수 있다.
+//           }}
+//         >
+//           +1
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
+
+// import React, { Component } from "react";
+
+// class Counter extends Component {
+//   // state의 초깃값 설정하기
+//   state = {
+//     number: 0,
+//     fixedNumber: 0,
+//   };
+
+//   render() {
+//     const { number, fixedNumber } = this.state;
+//     return (
+//       <div>
+//         <h1>{number}</h1>
+//         <h2>바뀌지 않는 값: {fixedNumber}</h2>
+//         <button
+//           onClick={() => {
+//             this.setState({ number: number + 1 }); // this.setState를 사용하여 state에 새로운 값을 넣을 수 있다.
+//           }}
+//         >
+//           +1
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
+
 import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props); // props를 오버라이딩
-
-    // state의 초깃값 설정하기
-    this.state = {
-      number: 0,
-      fixedNumber: 0,
-    };
-  }
+  // state의 초깃값 설정하기
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
 
   render() {
     const { number, fixedNumber } = this.state;
@@ -19,7 +77,15 @@ class Counter extends Component {
         <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 }); // this.setState를 사용하여 state에 새로운 값을 넣을 수 있다.
+            this.setState((prevState) => {
+              return {
+                number: number + 1,
+              };
+            }); // this.setState를 사용하여 state에 새로운 값을 넣을 수 있다.
+
+            this.setState((prevState) => ({
+              number: number + 1,
+            }));
           }}
         >
           +1
--- React/react-book/src/index.js
+++ React/react-book/src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Counter from "./Counter";
+import Say from "./Say";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <Say />
   </React.StrictMode>
 );
 
