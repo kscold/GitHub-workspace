884eaaceaade6137a577a3f9fac3ad50e0f6aef6
kscold <chan6502@gmail.com>
2024-01-01 22:02:53 +0900

    React Udpate

--- React/react-book/src/index.js
+++ React/react-book/src/index.js
@@ -1,15 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import Counter from "./Counter";
-import Say from "./Say";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import EventPractice from './EventPractice';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Say />
+    <EventPractice />
   </React.StrictMode>
 );
 
