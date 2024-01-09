63e6e6cc41e5f1ebf88f923752fa2d0e3b9eb287
kscold <chan6502@gmail.com>
2024-01-09 22:09:35 +0900

    python Update

---
+++ python/코딩클럽/김도윤 학생/입출력.py
@@ -0,0 +1,14 @@
+# 입력 출력의 줄임말
+int1 = int(input())
+int2 = int(input())
+
+
+# 변수는 그릇
+
+# 타입(형식)
+# intger 3 숫자형 변수 20억까지
+# string "문자열" 문자열 변수
+# boolean True 또는 False 참과 거짓을 표현하는 변수
+
+print(f"결과는 {int1 + int2}")
+print("결과는 " + str(int1 + int2))
