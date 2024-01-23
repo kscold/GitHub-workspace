eb138b7dad8969cdc239b8f65daf8cfc14186a26
kscold <chan6502@gmail.com>
2024-01-24 04:09:20 +0900

    Spring Update

--- Spring/Gradle/firstproject/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ Spring/Gradle/firstproject/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -85,13 +86,28 @@ public class ArticleController {
         // id에 맞는 데이터가 있으면 데이터를 반환하고 null이면 null를 반환
 
         // 2-2. 기존 데이터 값을 갱신하기
-        if (target != null) {
+        if (target != null) { // 데이터가 이미 존재하면
             articleRepository.save(articleEntitiy); // 엔티티를 DB에 저장(갱신)
         }
 
         // 3. 수정 결과 페이지로 리다이렉트하기
         return "redirect:/articles/" + articleEntitiy.getId();
+    }
 
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제 요청이 들어왔습니다!!");
+        // 1. 삭제할 대상 가져오기
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        // 2. 대상 엔티티 삭제하기
+        if (target != null) {
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제됐습니다!");
+        }
+
+        // 3. 결과 페이지로 리다이렉트하기
+        return "redirect:/articles";
     }
 
 }
--- Spring/Gradle/firstproject/firstproject/src/main/resources/data.sql
+++ Spring/Gradle/firstproject/firstproject/src/main/resources/data.sql
@@ -1,3 +1,8 @@
-INSERT INTO article(id, title, content) VALUES (1,'가가가가','1111');
-INSERT INTO article(id, title, content) VALUES (2,'나나나나','2222');
-INSERT INTo article(id, title, content) VALUES (3,'다다다다','3333');
+-- @GeneratedValue(strategy = GenerationType.IDENTITY)로 데이터베이스 기반으로 위임하고 나서 id key와 value를 삭제한다.
+-- INSERT INTO article(id, title, content) VALUES (1,'가가가가','1111');
+-- INSERT INTO article(id, title, content) VALUES (2,'나나나나','2222');
+-- INSERT INTo article(id, title, content) VALUES (3,'다다다다','3333');
+
+INSERT INTO article(title, content) VALUES ('가가가가', '1111');
+INSERT INTO article(title, content) VALUES ('나나나나', '2222');
+INSERT INTo article(title, content) VALUES ('다다다다', '3333');
