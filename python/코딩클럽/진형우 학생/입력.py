44f028d42792c97bc4768452fc9602424cca3671
kscold <chan6502@gmail.com>
2024-07-04 23:58:35 +0900

    코딩클럽 진행사항 업데이트
    진형우 학생 추가

---
+++ python/코딩클럽/진형우 학생/입력.py
@@ -0,0 +1,11 @@
+# 파이썬에서 입력은 input()이라는 명령어로 입력함
+# input()은 기본적으로 문자열(str)""로 입력을 받음
+
+# a = input()
+# b = input()
+# print(a + b)
+
+# 만약 정수로 입력을 받을려면 int()로 감싸주어서 형변환(casting)을 해주어야함
+a = int(input())
+b = int(input())
+print(a + b)
---
+++ python/코딩클럽/진형우 학생/자료형.py
@@ -0,0 +1,16 @@
+# 자료형(type)
+
+# str string 문자열 "" ''
+# int intger 정수 -1 0 1 2
+# float 실수 3.14
+# bool boolean True False
+
+
+print("안녕")
+print(type("안녕"))
+print(10)
+print(type(10))
+print(3.14)
+print(type(3.14))
+print(True)
+print(type(True))
